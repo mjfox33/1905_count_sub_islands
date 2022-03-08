393f3f0953c63dc14dce479f973ea0f84bff316d
mjfox33 <mjfox33@gmail.com>
2022-03-08 07:31:13 -0500

    much faster idea - only loop through g2 once

--- code_1905_count_sub_islands.py
+++ code_1905_count_sub_islands.py
@@ -1,39 +1,19 @@
 class Solution:
     def countSubIslands(self, grid1: list[list[int]], grid2: list[list[int]]) -> int:
+        n = len(grid2)
+        m = len(grid2[0])
+            
+        def dfs(row, col):
+            if not (0 <= row < n and 0 <= col < m and grid2[row][col] == 1):
+                return 1
+
+            grid2[row][col] = 0
+            is_sub_island = grid1[row][col]
+            is_sub_island &= dfs(row, col-1)
+            is_sub_island &= dfs(row, col+1)
+            is_sub_island &= dfs(row-1, col)
+            is_sub_island &= dfs(row+1, col)
+
+            return is_sub_island
         
-        def get_islands(grid):
-            islands = list()
-            n = len(grid)
-            m = len(grid[0])
-            for row in range(n):
-                for col in range(m):
-                    if grid[row][col] == 1:
-                        current_island = list()
-                        dfs((row, col), grid, current_island)
-                        islands.append(current_island)
-            return islands
-
-
-        def dfs(cell, grid, current_island):
-            row, col = cell
-            n = len(grid)
-            m = len(grid[0])
-            if row < 0 or row >= n or col < 0 or col >= m or grid[row][col] == 0:
-                return
-
-            grid[row][col] = 0
-            current_island.append((row, col))
-            dfs((row, col-1), grid, current_island)
-            dfs((row, col+1), grid, current_island)
-            dfs((row-1, col), grid, current_island)
-            dfs((row+1, col), grid, current_island)
-        
-        g1 = get_islands(grid1)
-        g2 = get_islands(grid2)
-
-        sub_islands = 0
-        for g2_island in g2:
-           for g1_island in g1:
-               if all(i in g1_island for i in g2_island):
-                   sub_islands += 1
-        return sub_islands
+        return sum(dfs(row, col) for row in range(n) for col in range(m) if grid2[row][col])
